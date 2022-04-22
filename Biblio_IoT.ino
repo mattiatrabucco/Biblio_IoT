f63de0d4b9d756ae10bfb76b93b9a89b59d9f3e0
mattiatrabucco <mattia.trabucco@hotmail.it>
2022-04-22 11:23:18 +0200

    Lettura NFC funzionante

--- Biblio_IoT.ino
+++ Biblio_IoT.ino
@@ -12,22 +12,26 @@
 
 #define SS1_PIN 42
 
-#define NR_OF_READERS   2
+#define NR_OF_READERS 2
 
-byte ssPins[] = {SS_PIN, SS1_PIN};
+byte ssPins[] = {SS1_PIN, SS_PIN};
 MFRC522 mfrc522[NR_OF_READERS];
 
 
 void setup() {
   Serial.begin(9600);
+
   SPI.begin(); // init SPI bus
-    for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+
+  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) 
+  {
     mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
     Serial.print(F("Reader "));
     Serial.print(reader);
     Serial.print(F(": "));
     mfrc522[reader].PCD_DumpVersionToSerial();
   }
+
   //LED setup:
   pinMode(GREENLED, OUTPUT);
   pinMode(REDLED, OUTPUT);
@@ -36,9 +40,8 @@ void setup() {
 }
 
 void loop() {
-for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     // Look for new cards
-
     if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
       Serial.print(F("Reader "));
       Serial.print(reader);
@@ -46,39 +49,37 @@ for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
       Serial.print(F(": Card UID:"));
       dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
       Serial.println();
-      //Serial.print(F("PICC type: "));
-      //MFRC522::PICC_Type piccType = mfrc522[reader].PICC_GetType(mfrc522[reader].uid.sak);
-      //Serial.println(mfrc522[reader].PICC_GetTypeName(piccType));
+      /*Serial.print(F("PICC type: "));
+      MFRC522::PICC_Type piccType = mfrc522[reader].PICC_GetType(mfrc522[reader].uid.sak);
+      Serial.println(mfrc522[reader].PICC_GetTypeName(piccType));*/
       
       String stringa="";
       char c='\n';
+
+      delay(28); //attendo risposta da python
       
       while(stringa==""){
             stringa = Serial.readStringUntil(c);
-            //Serial.println(stringa);
       }
       if (stringa == "OK"){
         digitalWrite(GREENLED, HIGH);
         digitalWrite(REDLED, LOW);
       }
 
-      delay(1000);
+      delay(800);
 
       digitalWrite(REDLED, HIGH);
       digitalWrite(GREENLED, LOW);
-      
+
       // Halt PICC
       mfrc522[reader].PICC_HaltA();
       // Stop encryption on PCD
       mfrc522[reader].PCD_StopCrypto1();
     } 
-  } 
-
+  }
 }
 
-/**
- * Helper routine to dump a byte array as hex values to Serial.
- */
+// Helper routine to dump a byte array as hex values to Serial.
 void dump_byte_array(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import serial
 # s = serial.Serial("/dev/serial/port1", 115200)
 # s.write(b"Hello from serial port")
 
-stringNFC=b"Reader 0: Card UID: AA 73 95 19\r\n"
+stringNFC=b"AA 73 95 19"
 
 #s = serial.Serial('COM3')
 s = serial.Serial('/dev/tty.usbmodem101')
 while True:
     res = s.readline()
     print(res)
-    if stringNFC == res:
-        print("OK")
-        s.write(b"OK\n")
+    if b"Card UID" in res:
+        if stringNFC in res:
+            print("OK")
+            s.write(b"OK\n")
+        else:
+            print("ERROR: non puoi entrare")
+            s.write(b"ERROR: non puoi entrare\n")
