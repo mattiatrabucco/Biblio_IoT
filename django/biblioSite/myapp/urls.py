8b029b9e5d515e9da5e352f58e6736b7fe4c1552
mattiatrabucco <mattia.trabucco@hotmail.it>
2022-06-15 14:36:48 +0200

    New urls for RESTful API

--- django/biblioSite/myapp/urls.py
+++ django/biblioSite/myapp/urls.py
@@ -1,9 +1,10 @@
+from sys import api_version
 from django.urls import path
 
 
 from . import views
 
-
+api_path="api/v1/"
 app_name='myapp'
 urlpatterns = [
     path('',views.index, name='index'),
@@ -14,4 +15,8 @@ urlpatterns = [
     path('add_student/', views.add_student, name='add_student'),
     path('remove_student/', views.remove_student, name='remove_student'),
     path('list_student/', views.list_student, name='list_student'),
+
+    path(api_path + "all_auth_cards/", views.all_auth_cards, name='auth_card'),
+    path(api_path + "auth_card/<str:facolta>/<str:card_id>/", views.auth_card, name='auth_card'),
+    path(api_path + "library/<str:biblioteca>/<str:card_id>/", views.library, name='library'),
 ]
