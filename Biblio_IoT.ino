17d0376842b7e4ed27962db876dec930e7282a15
mattiatrabucco <mattia.trabucco@hotmail.it>
2022-06-15 14:37:29 +0200

    New FSM and serial communication

--- Biblio_IoT.ino
+++ Biblio_IoT.ino
@@ -22,24 +22,43 @@ MFRC522 mfrc522[NR_OF_READERS];
 //Display requirements
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+//Variabile usata per controllare i dati a intervalli regolari
+unsigned long display_millis, bridge_millis;
+
+//FSM states
+int ser_state, f_ser_state;
+
+//In order to wait the python script
+bool var_state;
+
+//Saves the current reader ID
+uint8_t cur_reader;
+
 void setup() {
   Serial.begin(9600);
 
   SPI.begin(); // init SPI bus
 
+  //Var setup
+  bridge_millis = display_millis = millis();
+  ser_state = f_ser_state = 0;
+  var_state = true;
+  cur_reader = 0;
+
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) 
   {
     mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
-    Serial.print(F("Reader "));
-    Serial.print(reader);
-    Serial.print(F(": "));
-    mfrc522[reader].PCD_DumpVersionToSerial();
+    // Serial.print(F("Reader "));
+    // Serial.print(reader);
+    // Serial.print(F(": "));
+    // mfrc522[reader].PCD_DumpVersionToSerial();
+    // delay(1000);
   }
 
   //Display setup:
   lcd.init();                    //initialize
   lcd.backlight();               //turn on backlight
-  lcd.print("Biblio IoT"); //display startup message
+  lcd.print("Biblio IoT");       //display startup message
 
   //LED setup:
   pinMode(GREENLED, OUTPUT);
@@ -49,52 +68,64 @@ void setup() {
 }
 
 void loop() {
-  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
-    // Look for new cards
-    if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
-      Serial.print(F("Reader "));
-      Serial.print(reader);
-      // Show some details of the PICC (that is: the tag/card)
-      Serial.print(F(": Card UID:"));
-      dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
-      Serial.println();
-      /*Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = mfrc522[reader].PICC_GetType(mfrc522[reader].uid.sak);
-      Serial.println(mfrc522[reader].PICC_GetTypeName(piccType));*/
-      
-      String stringa="";
-      char c='\n';
 
-      delay(28); //waiting python script
-      
-      while(stringa==""){
-            stringa = Serial.readStringUntil(c);
-      }
-      if (stringa == "OK"){
+
+  if ((millis() - bridge_millis > 200) && !var_state) {
+    bridge_millis = millis();
+
+    uint8_t status_code = status_read();
+      if (status_code == 1){
         digitalWrite(GREENLED, HIGH);
         digitalWrite(REDLED, LOW);
 
+        display_millis = millis();
+
         lcd.clear();            //Clear display
         lcd.setCursor(0, 0);    //Write on the first row
-        if (reader == 0)
+        
+        if (cur_reader == 0)
         {
           lcd.print("Benvenuto,"); //Print title on external display
         }
-        else if (reader == 1) {
+        else if (cur_reader == 1) {
           lcd.print("Arrivederci,"); //Print title on external display
         }
         
         lcd.setCursor(0, 1);    //Write on the second row
-        dump_byte_array_to_lcd(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
+        dump_byte_array_to_lcd(mfrc522[cur_reader].uid.uidByte, mfrc522[cur_reader].uid.size);
       }
+      else if (status_code == 2) {
+        display_millis = millis();
 
-      delay(800);
+        lcd.clear();            //Clear display
+        lcd.setCursor(0, 0);    //Write on the first row
+        lcd.print("Errore");    //Print title on external display
+      }
+    
+    var_state = true; //we can now turn back on reading
+  }
 
-      lcd.clear(); //reset display
-      lcd.setCursor(0, 0);
+  if (millis() - display_millis > 3000) {
+    display_millis = millis();
 
-      digitalWrite(REDLED, HIGH);
-      digitalWrite(GREENLED, LOW);
+    lcd.clear(); //reset display
+    lcd.setCursor(0, 0);
+
+    digitalWrite(REDLED, HIGH); //restore LEDs
+    digitalWrite(GREENLED, LOW);
+  }
+
+  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+    // Look for new cards
+    if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial() && var_state) {
+      
+      Serial.print(reader);
+      dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size); //send card ID to serial
+      Serial.write(0xFF);
+      
+      // In order to wait the python script  
+      var_state = false;
+      cur_reader = reader;
 
       // Halt PICC
       mfrc522[reader].PICC_HaltA();
@@ -104,10 +135,31 @@ void loop() {
   }
 }
 
+uint8_t status_read(){
+  while(Serial.available()){
+    uint8_t c = Serial.read();
+    f_ser_state = 0;
+    if(c == 'O' && ser_state == 0)  f_ser_state = 1;
+    if(c == 'N' && ser_state == 0)  f_ser_state = 3;
+    if(c == 'K' && ser_state == 1)  f_ser_state = 2;
+    if(c == 'O' && ser_state == 3)  f_ser_state = 4;
+    ser_state = f_ser_state ;
+  }
+  if(ser_state == 2) {
+    f_ser_state = ser_state = 0;
+    return 1; //OK
+  }
+  if(ser_state == 4) {
+    f_ser_state = ser_state = 0;
+    return 2; //NO
+  }
+  return 0;
+}
+
 // Helper routine to dump a byte array as hex values to Serial.
 void dump_byte_array(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i] < 0x10 ? "0" : "");
     Serial.print(buffer[i], HEX);
   }
 }
